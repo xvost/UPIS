3f79c3c3049a1604fde5bee19c871a4c97de6fe1
Andrey Pereshein <apereshein@yandex-team.ru>
2020-04-04 14:49:45 +0300

    Correct proxy choice

--- start_install.py
+++ start_install.py
@@ -8,15 +8,21 @@ import configfile
 
 
 def config(ip, type):
+    if not type and ip == 'input':
+        ip = input('Тип сетей для настройки (ipv4|ipv6):\n')
     start_type = [interactive, configfile][type]
     start_type.start(ip)
 
 
 arguments = argparse.ArgumentParser()
-arguments.add_argument('type', choices=('ipv4', 'ipv6'), help='Type of subnet, default {ipv6}', default='ipv6')
 start_type = arguments.add_mutually_exclusive_group()
 start_type.add_argument('-i', action='store_false', help='Interactive mode')
 start_type.add_argument('-c', action='store_true', help='Use configfile ./data_ipv6.conf or ./data_ipv4.conf')
+arguments.add_argument('type',
+                       choices=('ipv4', 'ipv6','input'),
+                       help='Type of subnet, default {ipv6}',
+                       default='ipv6',
+                       )
 
 args = arguments.parse_args()
 
