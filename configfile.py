7e6275afb5ec78a661b6486cb0b68edd7cda5451
Andrey Pereshein <apereshein@yandex-team.ru>
2020-04-15 23:40:20 +0300

    Change config type

--- configfile.py
+++ configfile.py
@@ -6,31 +6,38 @@ def start(subnet_type):
 
 
 def ipv6():
+    import configobj
+
+    config = configobj.ConfigObj('data_ipv6.conf')
+
+    proxy = config['Proxy']
+    servers = config['Servers']
+
+    cron = proxy['cronstring']
+    hour = cron[0]
+    minute = cron[1]
+
     env = open('./inventory/env', 'w')
     env.write('proxyv6:\n  hosts:\n')
 
-    config_data = open('data_ipv6.conf', 'r')
-    login, password, count, net_type, rotate_type, rotate, cron = config_data.readline().split(' ')
-    minute = cron.split(',')[1].strip('\n')
-    hour = cron.split(',')[0]
-    if minute == '*': minute = '"*"'
-    if hour == '*': hour = '"*"'
-
-    for line in config_data:
-        if line == '':
-            continue
-        ip, ipv64, ipv6, root_passwd = line.split(' ')
+    for server in servers.keys():
+        server = servers[server]
+        if isinstance(server['proxynetwork'], list):
+            network = ', '.join(server['proxynetwork'])
+        else:
+            network = server['proxynetwork']
         env.write(str('\n'
                       '    server_{ip}:\n'
                       '      ansible_host: {ip}\n'
-                      '      ansible_ssh_user: root\n'
-                      '      ansible_ssh_pass: {root_passwd}\n'
+                      '      ansible_ssh_user: {user}\n'
+                      '      ansible_ssh_pass: {user_passwd}\n'
                       '      ipv4: {ip}\n'
                       '      nipv6: {ipv6}\n'
-                      '      nipv664: {ipv64}\n').format(ip=ip,
-                                                         root_passwd=root_passwd,
-                                                         ipv6=ipv6,
-                                                         ipv64=ipv64))
+                      '      nipv664: {ipv64}\n').format(ip=server['ip'],
+                                                         user=server['login'],
+                                                         user_passwd=server['password'],
+                                                         ipv6=network,
+                                                         ipv64=server['gatewayipv64']))
 
     env.write(str(('\n'
                    '  vars:\n'
@@ -40,41 +47,54 @@ def ipv6():
                    '    proxy_password: {password}\n'
                    '    rotate_type: {rotate_type}\n'
                    '    rotate: {rotate}\n'
-                   '    cron_hour: {hour}\n'
-                   '    cron_minute: {minute}').format(net_type=net_type,
-                                  count=count,
-                                  login=login,
-                                  password=password,
-                                  rotate=rotate,
-                                  rotate_type=rotate_type,
-                                  hour=hour,
-                                  minute=minute)))
+                   '    cron_hour: "{hour}"\n'
+                   '    cron_minute: "{minute}"').format(net_type=proxy['nettype'],
+                                                         count=proxy['quantityinsubnet'],
+                                                         login=proxy['login'],
+                                                         password=proxy['password'],
+                                                         rotate=proxy['rotation'],
+                                                         rotate_type=proxy['randomtype'],
+                                                         hour=hour,
+                                                         minute=minute)))
 
     env.close()
 
 
 def ipv4():
+    import configobj
+
+    config = configobj.ConfigObj('data_ipv4.conf')
+
+    proxy = config['Proxy']
+    servers = config['Servers']
+
     env = open('./inventory/env', 'w')
     env.write('proxyv4:\n  hosts:\n')
 
-    config_data = open('data_ipv4.conf', 'r')
-    login, password, count = config_data.readline().split(' ')
-
-    for line in config_data:
-        if line == '':
-            continue
-        ip, subnet, root_passwd = line.split(' ')
+    for server in servers:
+        server = servers[server]
+        if isinstance(server['proxynetwork'], list):
+            network = ', '.join(server['proxynetwork'])
+        else:
+            network = server['proxynetwork']
         env.write(str('\n'
                       '    server_{ip}:\n'
                       '      ansible_host: {ip}\n'
-                      '      ansible_ssh_user: root\n'
-                      '      ansible_ssh_pass: {root_passwd}\n'
-                      '      subnet: {subnet}').format(ip=ip, root_passwd=root_passwd, subnet=subnet))
+                      '      ansible_ssh_user: {user}\n'
+                      '      ansible_ssh_pass: {user_passwd}\n'
+                      '      subnet: {subnet}').format(ip=server['ip'],
+                                                       user=server['login'],
+                                                       user_passwd=server['password'],
+                                                       subnet=network))
 
     env.write(str(('\n'
                    '  vars:\n'
                    '    count: {count}\n'
-                   '    proxy_login: {login}\n'     
-                   '    proxy_password: {password}').format(count=count, login=login, password=password)))
+                   '    start_port: {port}\n'
+                   '    proxy_login: {login}\n'
+                   '    proxy_password: {password}').format(count=proxy['quantityinsubnet'],
+                                                            port=proxy['startport'],
+                                                            login=proxy['login'],
+                                                            password=proxy['password'])))
 
     env.close()
--- roles/setup_proxies_ipv4/files/scripts/setup_ipv4.py
+++ roles/setup_proxies_ipv4/files/scripts/setup_ipv4.py
@@ -5,13 +5,18 @@ import ipaddress
 import os
 import sys
 
-login, passwd, subnet = sys.argv[1:]
+login = sys.argv[1]
+passwd = sys.argv[2]
+subnet = sys.argv[3:][0].split(', ')
+startport = sys.argv[4]
+
+print(subnet)
 
 head = '''daemon
-echo maxconn 20000
-echo nserver 127.0.0.1
-echo nserver 8.8.8.8
-echo nserver 1.1.1.1
+maxconn 20000
+nserver 127.0.0.1
+nserver 8.8.8.8
+nserver 1.1.1.1
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65534
@@ -32,7 +37,7 @@ ulimit -u 600000
 rc_local_tail ='''
 sleep 5
 
-/root/3proxy/src/3proxy /root/3proxy/3proxy.cfg
+/root/3proxy/3proxy /root/3proxy/3proxy.cfg
 
 exit 0
 '''
@@ -41,20 +46,18 @@ proxy_string = 'proxy -s0 -n -a -p{port} -i{input} -e{output}\n'
 rc_local = '/sbin/ip addr add {ip} dev eth0 \n'
 
 
-
-def subnet_single(subnet, start):
+def subnet_single(subnets, start):
+    start=int(start)
     config = open('/root/3proxy/3proxy.cfg', 'w')
-    subnet = ipaddress.ip_network(subnet)
     config.write(head.format(login=login, passwd=passwd))
-    for e, addr in enumerate(subnet):
-        config.write(proxy_string.format(port=int(start)+e, input=addr, output=addr))
-        ip_add(addr)
+    for subnet in subnets:
+        subnet = ipaddress.ip_network(subnet)
+        for addr in subnet:
+            config.write(proxy_string.format(port=start, input=addr, output=addr))
+            start += 1
     config.close()
-    add_autoload(subnet=subnet)
+    add_autoload(subnet=subnets)
 
-def ip_add(ip):
-    intf_ip = subprocess.getoutput('/sbin/ip addr add {} dev eth0'.format(ip))
-    print(intf_ip)
 
 def add_autoload(subnet='', address=''):
     os.rename('/etc/rc.local', '/etc/rc.local.backup')
@@ -62,12 +65,13 @@ def add_autoload(subnet='', address=''):
     rc_local = open('/etc/rc.local', 'a')
     rc_local.write(rc_local_head)
     if subnet != '':
-        for addr in subnet:
-            rc_local.write('/sbin/ip addr add {ip} dev eth0 \n'.format(ip=addr))
+        for sub in subnet:
+            for addr in ipaddress.ip_network(sub):
+                rc_local.write('/sbin/ip addr add {ip} dev eth0 \n'.format(ip=addr))
     elif address != '':
         rc_local.write('/sbin/ip addr add {ip} dev eth0 \n'.format(ip=address))
     rc_local.write(rc_local_tail)
     rc_local.close()
     subprocess.call('chmod +x /etc/rc.local', shell=True)
 
-subnet_single(subnet, 30000)
+subnet_single(subnet, startport)
